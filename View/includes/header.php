b4048f58d8c869d9d3616ddca4bab87236acad55
ZooWie <55379205+DeRooDirk@users.noreply.github.com>
2021-01-28 17:00:03 +0100

    update repo
    added css file

--- View/includes/header.php
+++ View/includes/header.php
@@ -5,9 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Becode - Boiler plate MVC</title>
 </head>
 <body>
     <header>
-        <h1>Welcome to my site</h1>
+        <h1>Welcome to C.A.P</h1>
+        <h2>We Connect Awesome People</h2>
     </header>
