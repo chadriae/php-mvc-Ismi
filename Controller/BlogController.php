459bc8d40ae94151baac5cceb68d167dbdbdc5e7
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-02-03 16:50:36 +0100

    Working blog page

--- Controller/BlogController.php
+++ Controller/BlogController.php
@@ -14,10 +14,47 @@ class BlogController
 
         //load the view
         require 'View/blog.php';
+
+        if (isset($_POST['submit'])) {
+            $this->addPost();
+        }
     }
 
     public function __construct(DatabaseManager $databaseManager)
     {
         $this->databaseManager = $databaseManager;
+    }
+
+    public function addPost()
+    {
+        if (!empty($_POST['text'])) {
+            $this->newPostUserID = $_SESSION['student-id'];
+            $this->newPostAuthor = $_SESSION['first-name'];
+            $this->newPost = $_POST['text'];
+
+            $addNewPost = $this->databaseManager->dbconnection->query("INSERT into posts(student_id, first_name, post) VALUES ('$this->newPostUserID', '$this->newPostAuthor', '$this->newPost');");
+
+            if (!$addNewPost) {
+                var_dump($this->databaseManager->dbconnection->error);
+            } else {
+                header("location: index.php?page=blog&error=none");
+            }
+        }
+    }
+
+    public function getPosts()
+    {
+        $query = "SELECT * FROM posts;";
+        $posts = $this->databaseManager->dbconnection->query($query);
+        return $posts;
+    }
+
+    public function getAuthor($id)
+    {
+        $query = "SELECT first_name FROM student WHERE student_id = $id";
+        $statement = $this->databaseManager->dbconnection->prepare($query);
+        $statement->execute();
+        $author = $statement->fetch(\PDO::FETCH_ASSOC);
+        return $author;
     }
 }
--- View/blog.php
+++ View/blog.php
@@ -1,24 +1,32 @@
-<?php require 'includes/header-logged-in.php'?>
+<?php
+require 'includes/header-logged-in.php';
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
+error_reporting(E_ALL);
+$posts = $this->getPosts();
+?>
 
 
 <div class="container4">
-<h1 class="text-form" >Posts</h1>
-<p><i class="fas fa-user" aria-hidden="true"></i> Welcome to the community!</p>
+    <h1 class="text-form">Posts</h1>
+    <p><i class="fas fa-user" aria-hidden="true"></i> Welcome to the community!</p>
 
     <div>
-        <h3>Say Something...</h3>
-        </div>
-            <form>
-                <textarea name="text" cols="30" rows="5" placeholder="Create a post" required="">
-                </textarea>
-                <input class="btn btn-primary" type="submit" value="SUBMIT">
-            </form>
-        </div>
-    </div>  
-
-
-
+        <h3>Hello, <?= $_SESSION['first-name'] ?>.<br> Say Something...</h3>
     </div>
+    <form method="post">
+        <textarea name="text" cols="30" rows="5" placeholder="Create a post" required="">
+                </textarea>
+        <input class="btn btn-primary" type="submit" value="SUBMIT" name="submit">
+    </form>
+    <?php foreach ($posts as $post) : ?>
+        <p><?= $post['post'] ?> (written by <?= $post['first_name'] ?>)
+        </p>
+    <?php endforeach; ?>
+</div>
 
 
-        <?php require 'includes/footer.php'?>
+
+
+
+<?php require 'includes/footer.php' ?>
