a8ac6a059715a20cbff548a3a388781bd60144cc
180485 <ismiandhykawaty@icloud.com>
2021-02-04 19:32:29 +0100

    Footer styling

--- View/includes/footer.php
+++ View/includes/footer.php
@@ -1,10 +1,11 @@
     <footer>
         <div class="container2">
-            <p class="ismi small">
-                &copy; <i class="fab fa-behance"> Connect</i><?php echo date('Y') ?>
+            <p class="ismi small"> 
+                <div class="blue">
+                &copy; <img class="" src="./assets/img/logo.png" style= "width:100px" alt=""><?php echo date('Y') ?>
+            </div>
             </p>
         </div>
-
     </footer>
     </section>
     <script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
