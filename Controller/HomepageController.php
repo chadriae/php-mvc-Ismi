4bf9953d2427e83255899d72d1b7db5e389fa203
Christophe Adriaensens <christopheadriaensens@Christophes-MacBook-Pro.local>
2021-03-16 15:53:22 +0100

    Add Heroku db connection

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -14,6 +14,6 @@ class HomepageController
         // then the view will actually display them.
 
         //load the view
-        require 'View/index.php';
+        require 'View/homepage.php';
     }
 }
