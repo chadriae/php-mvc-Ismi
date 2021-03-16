72b3c7166ad13c1a69c272fbfdf73219193d7732
Christophe Adriaensens <christopheadriaensens@Christophes-MacBook-Pro.local>
2021-03-16 16:23:41 +0100

    Update mysql db connection heroku

--- config.php
+++ config.php
@@ -1,8 +1,15 @@
 <?php
 $config = [
-    'host' => "localhost",
-    'name' => "root",
-    'password' => "root",
-    'dbname' => "beconnect",
+    // 'host' => "localhost",
+    // 'name' => "root",
+    // 'password' => "root",
+    // 'dbname' => "beconnect",
+    // 'port' => 3306
+
+    'host' => "remotemysql.com",
+    'name' => "lbwYH9VpoY",
+    'password' => "0Gb1uFkGbs",
+    'dbname' => "lbwYH9VpoY",
     'port' => 3306
+
 ];
