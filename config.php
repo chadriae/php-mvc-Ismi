092914d25416535bafea0104b42b5ffdffe3a834
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-03-23 10:31:19 +0100

    Add db credentials

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 <?php
 $config = [
-    'host' => "localhost",
-    'name' => "root",
-    'password' => "root",
-    'dbname' => "beconnect",
+    // 'host' => "localhost",
+    // 'name' => "root",
+    // 'password' => "root",
+    // 'dbname' => "beconnect",
+    // 'port' => 3306
+
+    'host' => "remotemysql.com",
+    'name' => "lbwYH9VpoY",
+    'password' => "isxgwaTBxe",
+    'dbname' => "lbwYH9VpoY",
     'port' => 3306
 ];
